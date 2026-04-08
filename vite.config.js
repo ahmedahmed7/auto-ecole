054055f4bf85812105931481c79319ec897f56f7
ahmed hachicha <ahmedhachicha20@gmail.com>
2026-04-08 23:30:29 +0200

    change var

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/auth':        { target: 'http://148.230.124.150:8080/:8080', changeOrigin: true },
-      '/students':    { target: 'http://148.230.124.150:8080/:8080', changeOrigin: true },
-      '/instructors': { target: 'http://148.230.124.150:8080/:8080', changeOrigin: true },
-      '/lessons':     { target: 'http://148.230.124.150:8080/:8080', changeOrigin: true },
-      '/exams':       { target: 'http://148.230.124.150:8080/:8080', changeOrigin: true },
-      '/payments':    { target: 'http://148.230.124.150:8080/:8080', changeOrigin: true },
-      '/vehicules':   { target: 'http://148.230.124.150:8080/:8080', changeOrigin: true },
+      '/auth':        { target: 'http://148.230.124.150:8080', changeOrigin: true },
+      '/students':    { target: 'http://148.230.124.150:8080', changeOrigin: true },
+      '/instructors': { target: 'http://148.230.124.150:8080', changeOrigin: true },
+      '/lessons':     { target: 'http://148.230.124.150:8080', changeOrigin: true },
+      '/exams':       { target: 'http://148.230.124.150:8080', changeOrigin: true },
+      '/payments':    { target: 'http://148.230.124.150:8080', changeOrigin: true },
+      '/vehicules':   { target: 'http://148.230.124.150:8080', changeOrigin: true },
     },
   },
 });
