3087f19f3058e952d918880541f08d106b8ebedb
ahmed hachicha <ahmedhachicha20@gmail.com>
2026-08-17 21:53:03 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/auth':        { target: 'http://localhsot:8080', changeOrigin: true },
-      '/students':    { target: 'http://localhsot:8080', changeOrigin: true },
-      '/instructors': { target: 'http://localhsot:8080', changeOrigin: true },
-      '/lessons':     { target: 'http://localhsot:8080', changeOrigin: true },
-      '/exams':       { target: 'http://localhsot:8080', changeOrigin: true },
-      '/payments':    { target: 'http://localhsot:8080', changeOrigin: true },
-      '/vehicules':   { target: 'http://localhsot:8080', changeOrigin: true },
+      '/auth':        { target: 'http://186.241.21.210:8080', changeOrigin: true },
+      '/students':    { target: 'http://186.241.21.210:8080', changeOrigin: true },
+      '/instructors': { target: 'http://186.241.21.210:8080', changeOrigin: true },
+      '/lessons':     { target: 'http://186.241.21.210:8080', changeOrigin: true },
+      '/exams':       { target: 'http://186.241.21.210:8080', changeOrigin: true },
+      '/payments':    { target: 'http://186.241.21.210:8080', changeOrigin: true },
+      '/vehicules':   { target: 'http://186.241.21.210:8080', changeOrigin: true },
     },
   },
 });
